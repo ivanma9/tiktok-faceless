30c2b7bbf7b600691382fcaa37bda0f5c6f66f7d
Ivan Ma <ivanma819@gmail.com>
2026-03-14 17:06:33 -0500

    test: redirect SqliteSaver checkpoint DB to temp path in test suite

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,14 @@ Shared pytest fixtures and configuration.
 
 Additional fixtures will be added as stories are implemented.
 """
+import os
+import pytest
+
+
+@pytest.fixture(autouse=True, scope="session")
+def _checkpoint_db_in_memory(tmp_path_factory):
+    """Redirect SqliteSaver to a temp file so tests don't write checkpoints.db to repo root."""
+    tmp = tmp_path_factory.mktemp("checkpoints") / "checkpoints.db"
+    os.environ["CHECKPOINT_DB_PATH"] = str(tmp)
+    yield
+    os.environ.pop("CHECKPOINT_DB_PATH", None)
